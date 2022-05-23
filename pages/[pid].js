90aaeac8f39d057e6d304a7ddcf9e510f2f9a5b7
Ioan Zaharia <zaharia.ioan@gmail.com>
2022-05-23 17:13:13 +0200

    pre-rendering and fetching data basic

---
+++ pages/[pid].js
@@ -0,0 +1,84 @@
+import path from 'path';
+import fs from 'fs/promises';
+
+function ProductDetailPage(props) {
+  const { loadedProduct } = props;
+
+  // this check is needed if fallback: true
+  if (!loadedProduct) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <>
+      <h2>{loadedProduct.title}</h2>
+      <p>{loadedProduct.description}</p>
+    </>
+  );
+}
+
+async function getData() {
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  return data;
+}
+
+// for generatic static data for generated pages
+export async function getStaticProps(context) {
+  const { params } = context;
+  const productId = params.pid;
+
+  const data = await getData();
+
+  const product = data.products.find((product) => product.id === productId);
+
+  // this chech is needed if fallback: true,
+  // if the page with this params cannot be created
+  // then return notFound nextJs component logic
+  if (!product) {
+    return { notFound: true };
+  }
+
+  return {
+    props: {
+      loadedProduct: product,
+    },
+  };
+}
+
+// for dynamic generated pages
+export async function getStaticPaths() {
+  const data = await getData();
+
+  const ids = data.products.map((product) => product.id);
+  const pathsWithParams = ids.map((id) => ({ params: { pid: id } }));
+
+  return {
+    paths: pathsWithParams,
+    fallback: true,
+  };
+
+  // return {
+  //   paths: [
+  //     { params: { pid: 'p1' } },
+  //     // { params: { pid: 'p2' } },
+  //     // { params: { pid: 'p3' } },
+  //   ],
+  //   // fallback: false -> you need to add paths for every page
+  //   // also handle 404
+
+  //   // fallback: true -> you can specify paths for pages but also
+  //   // is creating paths on the fly when visited
+  //   // accepting any paramether
+
+  //   // fallback: 'blocking' -> work the same as "true" but no checking is needed
+  //   // (see ProductDetailPage function)
+  //   // is slower than 'fallback: true'
+
+  //   fallback: 'blocking',
+  // };
+}
+
+export default ProductDetailPage;
---
+++ pages/index.js
@@ -0,0 +1,47 @@
+import path from 'path';
+import fs from 'fs/promises';
+import Link from 'next/link';
+
+function HomePage(props) {
+  const { products } = props;
+
+  return (
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>
+          <Link href={`/${product.id}`}>{product.title}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+// for generatic static data for generated pages
+export async function getStaticProps(context) {
+  console.log('(Re-)Generating...');
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: '/no-data', // redirect to url (page)
+      },
+    };
+  }
+
+  if (data.products.length === 0) {
+    return { notFound: true }; // 404 page
+  }
+
+  return {
+    props: {
+      products: data.products,
+    },
+    // rebuild the static page in production every eg. 10 seconds
+    revalidate: 10,
+  };
+}
+
+export default HomePage;
